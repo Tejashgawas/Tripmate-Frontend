97b21aae05b353b9dc5a96c36afacff72c13c639
Tejashgawas <gawastejash@gmail.com>
2025-08-25 20:58:24 +0530

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { AuthProvider } from '@/contexts/AuthContext';
-
+import { Analytics } from "@vercel/analytics/next"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,6 +19,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <AuthProvider>
+            <Analytics/>
           {children}
         </AuthProvider>
       </body>
